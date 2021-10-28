d7eb03819e97801ddf822cc733c6ebba19c2d3b8
thienv29 <thienv29@gmail.com>
2021-10-28 18:44:41 +0700

    check sucess

--- Route/index.js
+++ Route/index.js
@@ -1,6 +1,9 @@
 import diemRoute from "./diemRoute.js";
 import tkbRoute from "./tkbRoute.js";
 function route(app) {
+  app.use("/", (req,res) => {
+    res.send('thanh')
+  });
   app.use("/tkb", tkbRoute);
   app.use("/diem", diemRoute);
 }
