4a8dfec8d5a178e6bde474fb4dc1ad6c47b2142e
thienv29 <thienv29@gmail.com>
2021-10-28 18:53:46 +0700

    check sucess

--- Route/index.js
+++ Route/index.js
@@ -1,10 +1,11 @@
 import diemRoute from "./diemRoute.js";
 import tkbRoute from "./tkbRoute.js";
 function route(app) {
+  
+  app.use("/tkb", tkbRoute);
+  app.use("/diem", diemRoute);
   app.use("/", (req,res) => {
     res.send('thanh')
   });
-  app.use("/tkb", tkbRoute);
-  app.use("/diem", diemRoute);
 }
 export default route;
